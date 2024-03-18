0c2d58bc4db90ef80605bdb0726487a85788cba5
Javier Gómez Pérez <gpjavierjob@gmail.com>
2024-03-18 13:20:54 -0300

    Adicionado el número del puerto externo como parámetro del script install-proxy.sh

--- deployment/install-proxy.sh
+++ deployment/install-proxy.sh
@@ -8,9 +8,10 @@ usage() {
   printf "servidor, su usuario y su contraseña. De estos, el nombre del cliente y sus direcciones IP\n"
   printf "son parámetos obligatorios y el usuario y la contraseña son opcionales. Si el usuario o\n"
   printf "la contraseña no son proporcionados, se generarán automáticamente.\n"
-  printf "Otros parámetros opcionales son la zona horaria para el servidor Squid, la etiqueta de la\n"
-  printf "imagen de docker de Squid a utilizar, un nombre para el contenedor de docker a crear\n"
-  printf "y si se desea desplegar el contenedor en MicroK8s.\n"
+  printf "Otros parámetros opcionales son el número del puerto, la zona horaria para el servidor Squid,\n"
+  printf "la etiqueta de la imagen de docker de Squid a utilizar, un nombre para el contenedor de\n"
+  printf "docker a crear y si se desea desplegar el contenedor en MicroK8s.\n"
+  printf "Si número del puerto no es proporcionado, se tomará el valor 3128 como predeterminado.\n"
   printf "Si la zona horaria no es proporcionada, se utilizará el valor actual del host.\n"
   printf "Si la etiqueta de la imagen de docker de Squid no es proporcionada, se utilizará\n"
   printf "ubuntu/squid:5.2-22.04_beta como valor predeterminado.\n"
@@ -30,6 +31,7 @@ usage() {
   printf "  -p --password\t\tContraseña del cliente (opcional).\n"
   printf "  -z --timezone\t\tZona horaria para Squid (opcional).\n"
   printf "  -t --image-tag\t\tEtiqueta de la imagen docker de Squid (opcional).\n"
+  printf "  -o --port\t\tNúmero del puerto (opcional).\n"
   printf "  -n --container-name\t\tNombre del contenedor docker de Squid a crear y ejecutar (opcional).\n"
   printf "  -k --microk8s\t\tIndica si se despliega el contenedor en MicroK8s (opcional).\n"
   printf "  -v --verbose\t\tExplica los pasos que se están ejecutando con más detalles.\n"
@@ -52,6 +54,7 @@ while [ "$#" -gt 0 ]; do
     -p|--password) password="$2" shift ;;
     -z|--timezone) timezone="$2" shift ;;
     -t|--image-tag) image_tag="$2" shift ;;
+    -o|--port) port="$2" shift ;;
     -n|--container-name) container_name="$2" shift ;;
     -k|--microk8s) microk8s=$TRUE ;;
     -v|--verbose) verbose=$TRUE ;;
@@ -75,6 +78,7 @@ if [ -r ${defaults_file_path} ]; then
   if [ -z "$password" ]; then password="$(grep --color=never -Po "^password=\K.*" $defaults_file_path || true)"; fi
   if [ -z "$timezone" ]; then timezone="$(grep --color=never -Po "^timezone=\K.*" $defaults_file_path || true)"; fi
   if [ -z "$image_tag" ]; then image_tag="$(grep --color=never -Po "^image_tag=\K.*" $defaults_file_path || true)"; fi
+  if [ -z "$port" ]; then port="$(grep --color=never -Po "^port=\K.*" $defaults_file_path || true)"; fi
   if [ -z "$container_name" ]; then container_name="$(grep --color=never -Po "^container_name=\K.*" $defaults_file_path || true)"; fi
   microk8s="$(grep --color=never -Po "^microk8s=\K.*" $defaults_file_path || true)";
 fi 
@@ -85,6 +89,7 @@ fi
 [ -z "$password" ] && password=$(openssl rand -base64 64 | tr -dc 'a-zA-Z0-9')
 [ -z "$timezone" ] && timezone=$(cat /etc/timezone)
 [ -z "$image_tag" ] && image_tag="ubuntu/squid:5.2-22.04_beta"
+[ -z "$port" ] && port="3128"
 [ -z "$container_name" ] && container_name="ubuntu-squid"
 
 # Ensure a valid value for client name
@@ -129,10 +134,10 @@ if [ $microk8s -eq $FALSE ]; then
   # Creating the squid container
   if [ $verbose -eq $TRUE ]; then
     echo ""
-    docker run -d --name "$container_name" -p 3128:3128 -e TZ="$timezone" -v "$host_snippets_dir:$cont_snippets_dir:ro" -v "$host_logs_dir:$cont_logs_dir" -v "$host_cache_dir:$cont_cache_dir" -v "$host_conf_file:$cont_conf_file:ro" "$image_tag"
+    docker run -d --name "$container_name" -p "$port:3128" -e TZ="$timezone" -v "$host_snippets_dir:$cont_snippets_dir:ro" -v "$host_logs_dir:$cont_logs_dir" -v "$host_cache_dir:$cont_cache_dir" -v "$host_conf_file:$cont_conf_file:ro" "$image_tag"
     echo -n "... "
   else
-    docker run -d --name "$container_name" -p 3128:3128 -e TZ="$timezone" -v "$host_snippets_dir:$cont_snippets_dir:ro" -v "$host_logs_dir:$cont_logs_dir" -v "$host_cache_dir:$cont_cache_dir" -v "$host_conf_file:$cont_conf_file:ro" "$image_tag" > /dev/null
+    docker run -d --name "$container_name" -p "$port:3128" -e TZ="$timezone" -v "$host_snippets_dir:$cont_snippets_dir:ro" -v "$host_logs_dir:$cont_logs_dir" -v "$host_cache_dir:$cont_cache_dir" -v "$host_conf_file:$cont_conf_file:ro" "$image_tag" > /dev/null
   fi
 
   if [ $? -gt 0 ]; then
@@ -150,15 +155,15 @@ else
   # First, remove username from image tag and then, prefix the Microk8s register address
   reg_tag="localhost:32000/$container_name:$(echo "$image_tag" | awk -F '/' '{print $2}' | awk -F ':' '{print $2}')"
 
-  # Get a random id
+  # Get a random id for deployment file name
   id="$(echo $(date +%s%N) | sha256sum | head -c 10)"
   
   # Deployment files path
   deploy_template_path="$script_dir/k8s-deployment.template.yaml"
   deploy_file_path="$script_dir/microk8s/k8s-deployment_$id.yaml"
 
-  # sed script command
-  sed_cmd='s|${CLIENT_NAME}|'"$client"'|g;s|${CONTAINER_NAME}|'"$container_name"'|g;s|${IMAGE_TAG}|'"$reg_tag"'|g'
+  # sed script command for variables substitution
+  sed_cmd='s|${CLIENT_NAME}|'"$client"'|g;s|${CONTAINER_NAME}|'"$container_name"'|g;s|${IMAGE_TAG}|'"$reg_tag"'|g;s|${PORT}|'"$port"'|g'
   # Preparing deployment file
   sed "-e" "$sed_cmd" "$deploy_template_path" > "$deploy_file_path"
 
@@ -187,7 +192,7 @@ else
   echo "The proxy has been deployed on MicroK8s."
 fi
 
-# Storing the deployment in installs file. When deploy_file_path value equals "0", the deployment
+# Storing the deployment in installs file. When deploy_file_path value equals NONE, the deployment
 # was on Docker, otherway, on MicroK8s
 echo "$client=$deploy_file_path" >> "$script_dir/installs"
 
